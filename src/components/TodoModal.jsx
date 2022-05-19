313769b65ee3750cab375e5a8118e76a878ae284
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-05-19 23:48:27 +0530

    fix: todo changes

--- src/components/TodoModal.jsx
+++ src/components/TodoModal.jsx
@@ -3,41 +3,50 @@ import { v4 as uuid } from "uuid";
 
 const TodoModal = () => {
   const [todo, setTodo] = useState("");
-  const [todoList, setTodolist] = useState([]);
+  const [todoList, setTodolist] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
   const todoHandler = (e) => {
+    console.log(e.target.value);
     setTodo(e.target.value);
   };
 
   const keyDownHandler = (e) => {
     if (e.key === "Enter") {
-      setTodolist([...todoList, {id: uuid() , todo , isCompleted: false}]);
-      localStorage.setItem("todos" , JSON.stringify(todoList))
+      localStorage.setItem("todos", JSON.stringify(todoList));
+      setTodolist([...todoList, { id: uuid(), todo, isCompleted: false }]);
       setTodo("");
     }
   };
 
   const checkBoxHandler = (item) => {
-   const items = todoList.map(todo => todo.id === item.id ? {...todo , isCompleted: !todo.isCompleted} : todo)
-   setTodolist(items)
+    const items = todoList.map((todo) =>
+      todo.id === item.id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodolist(items);
   };
 
   const deleteTodoHandler = (item) => {
-    const remainingItems = todoList.filter(todo => todo.id !== item.id)
-    setTodolist(remainingItems)
-  }
+    const remainingItems = todoList.filter((todo) => todo.id !== item.id);
+    setTodolist(remainingItems);
+  };
 
   useEffect(() => {
-    setTodolist(JSON.parse(localStorage.getItem("todos")))
-  },[])
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, []);
 
   return (
     <div className="todo-modal-container">
       <h3 className="todo-heading">Todo's</h3>
 
-      {todoList.map((item) => (
+      {todoList?.map((item) => (
         <div className="flex">
           <label
-            className={item.isCompleted ? "todo-check-text checked" : "todo-check-text"}
+            className={
+              item.isCompleted ? "todo-check-text checked" : "todo-check-text"
+            }
           >
             <input
               type="checkbox"
@@ -47,7 +56,12 @@ const TodoModal = () => {
             />
             {item.todo}
           </label>
-          <span className="material-symbols-outlined del-icon" onClick={() => deleteTodoHandler(item)}>delete</span>
+          <span
+            className="material-symbols-outlined del-icon"
+            onClick={() => deleteTodoHandler(item)}
+          >
+            delete
+          </span>
         </div>
       ))}
 
